172767bdb987c6c8e407799226d8d9a3cf40d410
Raphael <raphaelsantos10@gmail.com>
2023-11-25 21:40:10 -0300

    kotlin

---
+++ hello_world.kt
@@ -0,0 +1,7 @@
+package me.dio.helloworld         // 1 Definicao do pacote (algo muito comum em kotlin)
+
+fun main() {                        // 2 Ponto de entrada de um codigo kotlin , definido pela funcao[Main]
+    println("Hello, World!")        // 3 Imprime saida padrao de um texto pulando linha
+    println("ola")
+}
+
